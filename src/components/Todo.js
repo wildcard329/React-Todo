59838d8787720c1cb4d1dfc32bb3190187bb7253
wildcard329 <enkiduwildman@gmail.com>
2020-03-09 23:56:40 +0100

    linethrough is working

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import './Todo.css';
 
 const Todo = props => {
     return (
         <div
             onClick={() => props.toggleCompleted(props.todo.id)}
-            className={`todo${props.todo.completed ? 'completed' : ''}`}>    
+            className={`todo${props.todo.completed ? ' completed' : ''}`}>    
             <p>{props.todo.name}</p>
         </div>
     );
